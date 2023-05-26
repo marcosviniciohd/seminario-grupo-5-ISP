80a84099a906a6557aed82b74358783e344585b9
Marcos <marcosviniciohd@hotmail.com>
2023-05-25 23:41:19 -0300

    Desafio utilizando o principio ISP do SOLID

---
+++ src/br/edu/iftm/tributacao/Reajuste.java
@@ -0,0 +1,10 @@
+package br.edu.iftm.tributacao;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public interface Reajuste {
+    BigDecimal valor();
+    LocalDate data();
+    BigDecimal valorImpostoDeRenda();
+}
